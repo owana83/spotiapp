8c31b3828aaa125638102868d0768887fcf123aa
owana83 <frafalriv@gmail.com>
2019-01-02 13:25:06 +0100

    Ejercicio spotiapp angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar Rutas
+import { ROUTERS } from './app.routes';
+
+// Pipes
+import { NoimagePipe } from './pipes/noimage.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent,
+    NoimagePipe,
+    DomseguroPipe,
+    TarjetasComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot( ROUTERS, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http: HttpClient) {
+    console.log( 'Spotify Service Listo');
+   }
+
+  getQuery( query: string ) {
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDpvkNicOrxIA4Leq2I2fLl6SoJkc6TvyhVz1kV_BH_qXmNwOyEGyXZp0J2OFKNmz3JDgca-8PaS323s8Q'
+    });
+
+    return this.http.get(url, {headers});
+
+   }
+
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases?limit=20')
+        .pipe( map( data => data['albums'].items ));
+   }
+
+
+  getArtistas(termino: string) {
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+        .pipe( map( data => data['artists'].items ));
+
+  }
+
+  getArtista( id: string ) {
+
+    return this.getQuery(`artists/${ id }`);
+        // .pipe( map( data => data['artists'].items ));
+
+  }
+
+  getTopTracks( id: string ) {
+
+    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+        .pipe( map( data => data['tracks']));
+
+  }
+
+
+}
